0b8103dfdec76805b44e8f00f988e0b769cfe712
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-15 15:33:39 +0330

    create a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/header";
+
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="min-h-[100vh]">
+      <Header></Header>
+    </div>
   );
 };
 
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-[200px] border">
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
