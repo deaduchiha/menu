32e1f1a5c1b0959428a71e350ca8cb57d658441c
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-15 16:06:15 +0330

    complete header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
-    <header className="h-[200px] border">
-      <h1>header</h1>
+    <header className="h-[200px] bg-cover bg-header-background py-10">
+      <div className="container mx-auto max-w-xl ">
+        <h1 className="text-start text-white text-transform: uppercase tracking-[3px]">
+          restaurant menu
+        </h1>
+      </div>
     </header>
   );
 };
